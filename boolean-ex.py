4cba00fb86333f93d6158a9de53d505ed7c4bcbf
Udayaditya Singh <udayaditya.singh@gmail.com>
2019-09-12 12:01:06 +0530

    "numbers"

--- boolean-ex.py
+++ boolean-ex.py
@@ -1,6 +1,8 @@
 # A boolean is also an int . The bool type is a subclass of the int type and True and
 # False are its only instances
 
-issubclass(bool, int) # True
-isinstance(True, bool) # True
-isinstance(False, bool) # True
+issubclass(bool, int)  # True
+isinstance(True, bool)  # True
+isinstance(False, bool)  # True
+
+print(isinstance(False, bool))
