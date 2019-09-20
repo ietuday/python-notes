9fab54341cdc64682cc2b6885c72ce4215f81380
Udayaditya Singh <udayaditya.singh@gmail.com>
2019-09-20 11:32:49 +0530

    "print changes"

---
+++ print-test.py
@@ -0,0 +1,7 @@
+my_name = "udayaditya"
+print("Hello", my_name)
+print('This is a string {}'.format('Inserted'))
+print('This {} {} {}'.format('fox', 'brown', 'quick'))
+print('This {2} {1} {0}'.format('fox', 'brown', 'quick'))
+# print('This {0} {0} {0}'.format('fox', 'brown', 'quick'))
+print('This {q} {b} {f}'.format(f='fox', b='brown', q='quick'))
