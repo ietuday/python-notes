b288b87a7121723330a5e48f7413e0ef6f93a0fb
Udayaditya Singh <udayaditya.singh@gmail.com>
2019-09-21 08:23:33 +0530

    "list test"

---
+++ list-test.py
@@ -0,0 +1,19 @@
+# my_list = [1, 2, 3]
+# my_list1 = ['UDAY', 100, 23.2]
+# my_list2 = ['Test', 55, 25.89]
+# print(len(my_list))
+# print(my_list1[0])
+# print(my_list1[1:])
+# print(my_list1 + my_list2)
+# my_list1[0] = "uday"
+# my_list1.append(6)
+# my_list1.pop()
+# my_list1.pop(0)
+# print(my_list1)
+
+new_list = ['a', 'e', 'x', 'b', 'c']
+num_list = [4, 1, 8, 3]
+new_list.sort()
+num_list.reverse()
+print(new_list)
+print(num_list)
